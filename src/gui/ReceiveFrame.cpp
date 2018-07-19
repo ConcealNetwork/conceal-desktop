6539c6fd101a2c7d2d153164977fb069c6cec7ed
darkharvest <kahthan@gmail.com>
2018-07-19 14:19:17 +0530

    show mnemonic seed on key tab

--- src/gui/ReceiveFrame.cpp
+++ src/gui/ReceiveFrame.cpp
@@ -10,10 +10,13 @@
 #include <Common/Base58.h>
 #include "Common/StringTools.h"
 #include "ReceiveFrame.h"
+#include "CryptoNoteCore/Account.h"
+#include "Mnemonics/electrum-words.h"
 #include "CurrencyAdapter.h"
 #include "WalletAdapter.h"
-
 #include "ui_receiveframe.h"
+
+#include <string>
 
 namespace WalletGui {
 
@@ -42,9 +45,24 @@ void ReceiveFrame::walletOpened(int _error) {
   std::string secretKeysData = std::string(reinterpret_cast<char*>(&keys.spendSecretKey), sizeof(keys.spendSecretKey)) + std::string(reinterpret_cast<char*>(&keys.viewSecretKey), sizeof(keys.viewSecretKey));
   QString privateKeys = QString::fromStdString(Tools::Base58::encode_addr(CurrencyAdapter::instance().getAddressPrefix(), secretKeysData));
 
+  // check if the wallet is deterministic
+  // we do this by generating a view key from the spend key and them comparing it to the existing view key
+  Crypto::PublicKey unused_dummy_variable;
+  Crypto::SecretKey deterministic_private_view_key;
+  std::string mnemonic_seed = "";
+  CryptoNote::AccountBase::generateViewFromSpend(keys.spendSecretKey, deterministic_private_view_key, unused_dummy_variable);
+  bool deterministic_private_keys = deterministic_private_view_key == keys.viewSecretKey;
+  
+  if (deterministic_private_keys){
+  crypto::ElectrumWords::bytes_to_words(keys.spendSecretKey, mnemonic_seed, "English");
+  } else {
+     mnemonic_seed = "Your wallet does not support the use of a mnemonic seed. Please create a new wallet." 
+  }
+
   m_ui->m_keyEdit->setText(privateKeys);
   m_ui->m_keyEdit_3->setText(QString::fromStdString(Common::podToHex(keys.spendSecretKey)));
   m_ui->m_keyEdit_4->setText(QString::fromStdString(Common::podToHex(keys.viewSecretKey)));
+  m_ui->m_seed->setText(QString::fromStdString(mnemonic_seed));  
 }
 
 void ReceiveFrame::walletClosed() {
