59f8865e1808ae49f466328b553d4f5d2e59ef75
darkharvest <kahthan@gmail.com>
2019-01-16 09:48:48 +0530

    fix import seed file path dialog

--- src/gui/importseed.cpp
+++ src/gui/importseed.cpp
@@ -1,9 +1,7 @@
 // Copyright (c) 2011-2017 The Cryptonote developers
 // Copyright (c) 2018 The Circle Foundation
-//  
-// Copyright (c) 2018 The Circle Foundation
 // Distributed under the MIT/X11 software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file COPYING or http://www.opensource.org/licenses/mit-license.php
 
 #include <QApplication>
 #include <QFileDialog>
@@ -15,8 +13,6 @@ namespace WalletGui {
 
 ImportSeed::ImportSeed(QWidget* _parent) : QDialog(_parent), m_ui(new Ui::ImportSeed) {
   m_ui->setupUi(this);
-
-
 }
 
 ImportSeed::~ImportSeed() {
