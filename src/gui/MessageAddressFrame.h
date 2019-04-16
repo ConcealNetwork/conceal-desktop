12510bafa12e45910c50011be36cd45f264612b0
krypt0x <35565671+krypt0x@users.noreply.github.com>
2019-04-17 00:13:14 +0100

    Copyright 2019
    Granting rights to Conceal Network on regards of The Circle Foundation

--- src/gui/MessageAddressFrame.h
+++ src/gui/MessageAddressFrame.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2011-2016 The Cryptonote developers
 // Copyright (c) 2015-2016 XDN developers
-// Copyright (c) 2018 The Circle Foundation
+// Copyright (c) 2018 The Circle Foundation & Conceal Devs
+// Copyright (c) 2019 Conceal Network & Conceal Devs
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
