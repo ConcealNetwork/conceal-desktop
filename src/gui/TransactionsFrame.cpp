65150e083a14975253a4562fad7cd28b64c25607
cryptokatz <kahthan@gmail.com>
2019-08-29 15:31:43 +0530

    Reduce font size

--- src/gui/TransactionsFrame.cpp
+++ src/gui/TransactionsFrame.cpp
@@ -27,7 +27,7 @@ TransactionsFrame::TransactionsFrame(QWidget* _parent) : QFrame(_parent), m_ui(n
   m_ui->setupUi(this);
   m_ui->m_transactionsView->setModel(m_transactionsModel.data());
   m_ui->m_transactionsView->header()->setSectionResizeMode(TransactionsModel::COLUMN_STATE, QHeaderView::Fixed);
-  m_ui->m_transactionsView->header()->resizeSection(TransactionsModel::COLUMN_STATE, 15);
+  m_ui->m_transactionsView->header()->resizeSection(TransactionsModel::COLUMN_STATE, 25);
   m_ui->m_transactionsView->header()->resizeSection(TransactionsModel::COLUMN_DATE, 140);
   m_ui->m_transactionsView->header()->resizeSection(TransactionsModel::COLUMN_HASH, 300);
   m_ui->m_transactionsView->header()->moveSection( 3, 5 );
@@ -35,7 +35,7 @@ TransactionsFrame::TransactionsFrame(QWidget* _parent) : QFrame(_parent), m_ui(n
   int id2 = QFontDatabase::addApplicationFont(":/fonts/Lato-Regular.ttf");
   QFont font2;
   font2.setFamily("Lato");
-  font2.setPixelSize(15);
+  font2.setPixelSize(14);
   m_ui->m_transactionsView->setFont(font2);
 
 }
