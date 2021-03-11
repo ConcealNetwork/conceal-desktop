1105b7d97436f2287a912221e75a2fc6333d8642
AxVultis <axvultis@gmail.com>
2021-03-11 20:02:38 +0100

    Update UpdateManager

--- src/UpdateManager.h
+++ src/UpdateManager.h
@@ -1,39 +1,34 @@
 // Copyright (c) 2016 The Karbowanec developers
 // Copyright (c) 2018 The Circle Foundation & Conceal Devs
 // Copyright (c) 2018-2019 Conceal Network & Conceal Devs
-//  
+//
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef UPDATE_H
 #define UPDATE_H
 
-#include <QObject>
 #include <QNetworkAccessManager>
-#include <QNetworkRequest>
 #include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QObject>
 #include <QUrl>
 
 class Updater : public QObject
 {
-    Q_OBJECT
+  Q_OBJECT
 public:
-    explicit Updater(QObject *parent = 0);
+  explicit Updater(QObject* parent = 0);
 
-    ~Updater() {
-        delete manager;
-    }
+  ~Updater() { delete manager; }
 
-    void checkForUpdate();
+  void checkForUpdate();
 
-signals:
-    
 public slots:
-    void replyFinished (QNetworkReply *reply);
+  void replyFinished(QNetworkReply* reply);
 
 private:
-   QNetworkAccessManager *manager;
-
+  QNetworkAccessManager* manager;
 };
 
-#endif // UPDATE_H
+#endif  // UPDATE_H
