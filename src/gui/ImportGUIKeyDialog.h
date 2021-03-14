3d90190bbde429e9900819bd218958a27237b3ba
AxVultis <axvultis@gmail.com>
2021-03-14 22:53:48 +0100

    Update ImportGUIKeyDialog styles

--- src/gui/ImportGUIKeyDialog.h
+++ src/gui/ImportGUIKeyDialog.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2011-2017 The Cryptonote developers
 // Copyright (c) 2018 The Circle Foundation & Conceal Devs
-// Copyright (c) 2018-2019 Conceal Network & Conceal Devs
+// Copyright (c) 2018-2021 Conceal Network & Conceal Devs
+//
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -8,24 +9,33 @@
 
 #include <QDialog>
 
-namespace Ui {
+#include "EditableStyle.h"
+
+namespace Ui
+{
   class ImportGUIKeyDialog;
 }
 
-namespace WalletGui {
-
-  class ImportGUIKeyDialog : public QDialog {
+namespace WalletGui
+{
+  class ImportGUIKeyDialog : public QDialog, public EditableStyle
+  {
     Q_OBJECT
 
-    public:
-      explicit ImportGUIKeyDialog(QWidget* _parent);
-      ~ImportGUIKeyDialog();
-      QString getKeyString() const;
-      QString getFilePath() const;
-      void setStyles(int change);
+  public:
+    explicit ImportGUIKeyDialog(QWidget *_parent);
+    ~ImportGUIKeyDialog();
+    QString getKeyString() const;
+    QString getFilePath() const;
 
-    private:
-      QScopedPointer<Ui::ImportGUIKeyDialog> m_ui;
-      Q_SLOT void selectPathClicked();
+  protected:
+    QList<QWidget *> getWidgets() override;
+    QList<QPushButton *> getButtons() override;
+    QList<QLabel *> getLabels() override;
+    void applyStyles() override;
+
+  private:
+    QScopedPointer<Ui::ImportGUIKeyDialog> m_ui;
+    Q_SLOT void selectPathClicked();
   };
-}
+}  // namespace WalletGui
