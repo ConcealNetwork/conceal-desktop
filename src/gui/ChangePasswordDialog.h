abb26946ee5dda5e69a5dbfdc05ec140ce96fc21
AxVultis <axvultis@gmail.com>
2021-03-14 19:41:23 +0100

    Update ChangePasswordDialog styles

--- src/gui/ChangePasswordDialog.h
+++ src/gui/ChangePasswordDialog.h
@@ -1,15 +1,15 @@
 // Copyright (c) 2011-2017 The Cryptonote developers
 // Copyright (c) 2018 The Circle Foundation & Conceal Devs
-// Copyright (c) 2018-2019 Conceal Network & Conceal Devs
-//  
-// Copyright (c) 2018 The Circle Foundation & Conceal Devs
-// Copyright (c) 2018-2019 Conceal Network & Conceal Devs
+// Copyright (c) 2018-2021 Conceal Network & Conceal Devs
+//
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#pragma  once
+#pragma once
 
 #include <QDialog>
+
+#include "EditableStyle.h"
 
 namespace Ui {
 class ChangePasswordDialog;
@@ -17,16 +17,21 @@ class ChangePasswordDialog;
 
 namespace WalletGui {
 
-class ChangePasswordDialog : public QDialog {
+class ChangePasswordDialog : public QDialog, EditableStyle {
   Q_OBJECT
   Q_DISABLE_COPY(ChangePasswordDialog)
 
 public:
-  ChangePasswordDialog(QWidget* _parent);
+  explicit ChangePasswordDialog(QWidget* _parent);
   ~ChangePasswordDialog();
-  void setStyles(int change);
   QString getNewPassword() const;
   QString getOldPassword() const;
+
+protected:
+  QList<QWidget *> getWidgets() override;
+  QList<QPushButton *> getButtons() override;
+  QList<QLabel *> getLabels() override;
+  void applyStyles() override;
 
 private:
   QScopedPointer<Ui::ChangePasswordDialog> m_ui;
