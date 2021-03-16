294ddc02921465fadfd910e2d2d24008bc923adf
AxVultis <axvultis@gmail.com>
2021-03-16 18:50:30 +0100

    Update NewPasswordDialog styles

--- src/gui/NewPasswordDialog.h
+++ src/gui/NewPasswordDialog.h
@@ -1,9 +1,7 @@
 // Copyright (c) 2011-2017 The Cryptonote developers
 // Copyright (c) 2018 The Circle Foundation & Conceal Devs
-// Copyright (c) 2018-2019 Conceal Network & Conceal Devs
-//  
-// Copyright (c) 2018 The Circle Foundation & Conceal Devs
-// Copyright (c) 2018-2019 Conceal Network & Conceal Devs
+// Copyright (c) 2018-2021 Conceal Network & Conceal Devs
+//
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -11,26 +9,34 @@
 
 #include <QDialog>
 
-namespace Ui {
-class NewPasswordDialog;
+#include "EditableStyle.h"
+
+namespace Ui
+{
+  class NewPasswordDialog;
 }
 
-namespace WalletGui {
-  
-class NewPasswordDialog : public QDialog {
-  Q_OBJECT
-  Q_DISABLE_COPY(NewPasswordDialog)
+namespace WalletGui
+{
+  class NewPasswordDialog : public QDialog, public EditableStyle
+  {
+    Q_OBJECT
+    Q_DISABLE_COPY(NewPasswordDialog)
 
-public:
-  explicit NewPasswordDialog(QWidget* _parent);
-  ~NewPasswordDialog();
-  void setStyles(int change);
-  QString getPassword() const;
+  public:
+    explicit NewPasswordDialog(QWidget *_parent);
+    ~NewPasswordDialog();
+    QString getPassword() const;
 
-private:
-  QScopedPointer<Ui::NewPasswordDialog> m_ui;
+    QList<QWidget *> getWidgets() override;
+    QList<QPushButton *> getButtons() override;
+    QList<QLabel *> getLabels() override;
+    void applyStyles() override;
 
-  Q_SLOT void checkPassword(const QString& _password);
-};
+  private:
+    QScopedPointer<Ui::NewPasswordDialog> m_ui;
 
-}
+    Q_SLOT void checkPassword(const QString &_password);
+  };
+
+}  // namespace WalletGui
