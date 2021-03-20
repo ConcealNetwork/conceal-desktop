74e9a67cdca569b8e405fb446adc347da2b79163
AxVultis <axvultis@gmail.com>
2021-03-20 17:00:03 +0100

    Update TransactionDetailsDialog styles

--- src/gui/TransactionDetailsDialog.h
+++ src/gui/TransactionDetailsDialog.h
@@ -1,9 +1,7 @@
 // Copyright (c) 2011-2017 The Cryptonote developers
 // Copyright (c) 2018 The Circle Foundation & Conceal Devs
-// Copyright (c) 2018-2019 Conceal Network & Conceal Devs
-//  
-// Copyright (c) 2018 The Circle Foundation & Conceal Devs
-// Copyright (c) 2018-2019 Conceal Network & Conceal Devs
+// Copyright (c) 2018-2021 Conceal Network & Conceal Devs
+//
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -11,23 +9,32 @@
 
 #include <QDialog>
 
-namespace Ui {
-class TransactionDetailsDialog;
+#include "EditableStyle.h"
+
+namespace Ui
+{
+  class TransactionDetailsDialog;
 }
 
-namespace WalletGui {
+namespace WalletGui
+{
+  class TransactionDetailsDialog : public QDialog, public EditableStyle
+  {
+    Q_OBJECT
+    Q_DISABLE_COPY(TransactionDetailsDialog)
 
-class TransactionDetailsDialog : public QDialog {
-  Q_OBJECT
-  Q_DISABLE_COPY(TransactionDetailsDialog)
+  public:
+    TransactionDetailsDialog(const QModelIndex &_index, QWidget *_parent);
+    ~TransactionDetailsDialog();
 
-public:
-  TransactionDetailsDialog(const QModelIndex &_index, QWidget* _parent);
-  ~TransactionDetailsDialog();
+    QList<QWidget *> getWidgets() override;
+    QList<QPushButton *> getButtons() override;
+    QList<QLabel *> getLabels() override;
+    void applyStyles() override;
 
-private:
-  QScopedPointer<Ui::TransactionDetailsDialog> m_ui;
-  const QString m_detailsTemplate;
-};
+  private:
+    QScopedPointer<Ui::TransactionDetailsDialog> m_ui;
+    const QString m_detailsTemplate;
+  };
 
-}
+}  // namespace WalletGui
