4fc24e1619dbe7171992230131a3760a28613300
AxVultis <axvultis@gmail.com>
2021-06-12 12:29:12 +0200

    Update appimage script

--- appimage/create-appimage.sh
+++ appimage/create-appimage.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 if ! test -f "linuxdeploy-x86_64.AppImage"; then
     wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 fi
 if ! test -f "linuxdeploy-plugin-qt-x86_64.AppImage"; then
     wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
 fi
-chmod u+x *.AppImage
+chmod u+x ./*.AppImage
 cp ../build/release/CONCEAL-GUI .
 mkdir -p usr/share/icons
 mkdir -p usr/lib
